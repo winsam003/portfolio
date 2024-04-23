bb53b7df4656ab952c1e88e478f0fd3e34c26f57
[winsam] <winsam003@gmail.com>
2024-04-24 00:46:03 +0900

    포트폴리오 홈 화면 완성

--- public/index.js
+++ public/index.js
@@ -1,5 +1,56 @@
 "use strict"
 
+function fa_google(){
+    window.open('winsam003@gmail.com');
+}
+function github(){
+    window.open('https://github.com/winsam003');
+}
+function blogger(){
+    window.open('https://velog.io/@winsam003/posts');
+}
+
 function MVC_model(){
     window.open('https://github.com/simsim916/coinJung_CookieRun');
+}
+
+function header_portfolio() {
+    var element = document.getElementById("sec01");
+    if (element) {
+        element.scrollIntoView({
+            behavior: 'smooth'
+        });
+    }
+}
+function header_About() {
+    var element = document.getElementById("sec02");
+    if (element) {
+        element.scrollIntoView({
+            behavior: 'smooth'
+        });
+    }
+}
+function header_Project() {
+    var element = document.getElementById("sec03");
+    if (element) {
+        element.scrollIntoView({
+            behavior: 'smooth'
+        });
+    }
+}
+function header_Update() {
+    var element = document.getElementById("sec04");
+    if (element) {
+        element.scrollIntoView({
+            behavior: 'smooth'
+        });
+    }
+}
+function header_Baord() {
+    var element = document.getElementById("sec05");
+    if (element) {
+        element.scrollIntoView({
+            behavior: 'smooth'
+        });
+    }
 }
